df8ae21999dcbf5afefdb7c316d93ec72693f601
Timothy Miller <mr.tim.miller123@gmail.com>
2016-04-16 21:26:51 -0600

    Now displays a blank tic tac toe board

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "])
+  division = "|"
+  row = "-----------"
+  puts board[0] + division + board[1] + division + board[2]
+  puts row
+  puts board[3] + division + board[4] + division + board[5]
+  puts row
+  puts board[6] + division + board[7] + division + board[8]
+end
+
+
+display_board
